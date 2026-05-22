4fd6cb98cc2add53b30461732a8e57f29450c2e6
mangeky00 <mangeky00@outlook.com>
2026-05-22 12:47:09 +0300

    Delete CC.bat

--- CC.bat
+++
@@ -1,8 +0,0 @@
-@echo off
-set ANTHROPIC_BASE_URL=http://localhost:20128/v1
-set ANTHROPIC_AUTH_TOKEN=sk-cb17793ccac01503-b033dd-9ae88959
-set ANTHROPIC_API_KEY=
-set ANTHROPIC_MODEL=kr/claude-sonnet-4.5
-set ANTHROPIC_SMALL_FAST_MODEL=kr/claude-sonnet-4.5
-set CLAUDE_CODE_DISABLE_NONESSENTIAL_TRAFFIC=1
-claude %*
